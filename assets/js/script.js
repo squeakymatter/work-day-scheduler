97d9b03aa632726da09a4bef52e8c2a627d49bdb
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-01-09 05:44:18 -0800

    adds date to header and CSS grid for time blocks

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//display current day at top of calendar
+
+//create timeblocks for business hours (9am - 5pm)
+//use bootstrap grid for this. 
+// also, note style.css includes classes for timeblock, row, hour, past, present, future, saveBtn, so assign classes accordingly probably using jQuery in some cases...
+
+
+
+//color-code timeblock to indicate past, present or future
+
+
+
+//click in time block to enter an event
+
+
+//click save button for time block to save to local storage
+
+
+//when page is refreshed, saved event persists.
